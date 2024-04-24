ba32e33a0fe8ffd11cb8ad35b25ceb2ab7287356
Pathikrit Bhowmick <pathikritbhowmick@msn.com>
2024-04-24 15:12:25 -0400

    Handle Intermittent

--- vestaboard.js
+++ vestaboard.js
@@ -151,7 +151,7 @@ export class Vestaboard {
       {to: '&', from: ['And']},
       {to: '', from: ['Increasing', 'Becoming', 'Decreasing', 'Gradual', 'Patchy', 'Areas Of', 'Freezing']},
       {to: 'Heavy', from: ['Widespread']},
-      {to: 'Light ', from: ['Lt ', 'Very Light', 'Slight Chance', 'Slight Light', 'Slight Chance Light', 'Chance', 'Isolated', 'Scattered', 'Chance Light', 'Periods Of Light', 'Slight Chance Very Light', 'Intermittent Light']},
+      {to: 'Light ', from: ['Lt ', 'Very Light', 'Slight Chance', 'Slight Light', 'Slight Chance Light', 'Chance', 'Isolated', 'Scattered', 'Chance Light', 'Periods Of Light', 'Slight Chance Very Light', 'Intermittent Light', 'Intermittent']},
       {to: 'Rain', from: ['Rain Showers', 'Spray', 'Rain Fog', 'Showers']},
       {to: 'Snow', from: ['Snow Showers', 'Wintry Mix', 'Flurries']},
       {to: 'Tstms ', from: ['Thunderstorms']}
